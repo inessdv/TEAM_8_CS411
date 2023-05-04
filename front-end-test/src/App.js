afb54497bf118a8d8cf78817e521bf06684d5763
rou <rouliao0258@gmail.com>
2023-05-04 17:32:11 -0400

    displays current temperature in nyc

--- front-end-test/src/App.js
+++ front-end-test/src/App.js
@@ -13,6 +13,19 @@ import Profile from "./pages/Profile";
 
 function App() {
 
+  const apiKey = '15aeee8ccbc94a419df212006230405';
+  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=New%20York`;
+  
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {const temperature = data.current.temp_f;
+      const temperatureDisplay = document.getElementById("temperature-display");
+      temperatureDisplay.innerText = `${temperature}°F`;})
+    .catch(error => console.error(error));
+    
+  
+
+
   return (
     <Router>
       <Routes>
